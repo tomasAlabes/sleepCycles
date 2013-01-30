9c0299314be9de5e48a36183264cadb4a31adc01
Tomas Alabes <tomas.alabes@gmail.com>
2013-01-30 07:51:13 -0300

    timepicker

--- js/app/main.js
+++ js/app/main.js
@@ -1,3 +1,13 @@
 require(['jquery', 'nightSky'], function(){
 
+    $('#upTime').timepicker().on('update.timepicker', function(e) {
+        console.log('The time is ' + e.time);
+    });
+
+    $('#sleepTime').timepicker().on('update.timepicker', function(e) {
+        console.log('The time is ' + e.time);
+    });
+
+    $('.calculateBtn').css('margin-left', '25px');
+
 });
